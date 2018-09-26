216d030610adb5bd8a2f657a847c8df439337a7b
Khaja Naquiuddin <naquiuddin@gmail.com>
2018-09-26 15:08:50 +0530

    Added blurred course cards

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import { Link } from 'react-static';
-
-const courses = [
-  {
-    title: 'Programming and Computer Science Foundation',
-    subTitle: 'Level 1',
-    tags: [
-      'C Language',
-      'Python',
-      'SQL',
-      'Command Line',
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'CSS',
-      'HTML',
-    ],
-    description:
-      'You will learn how to think algorithmically and solve problems efficiently. You will go through topics like abstraction, algorithms, data structures, encapsulation, resource management, security, software engineering and web development. Languages you learn include C, Python, SQL, and JavaScript plus CSS and HTML',
-    detailsLink: '/programming-and-computer-science-foundation',
-  },
-  {
-    title: 'Advanced Algorithms and Problem Solving',
-    subTitle: 'Level 2',
-    tags: [''],
-    description: 'Every serious programmer should know Data Structures and Algorithms. You will learn how to think algorithmically and solve problems efficiently.',
-    detailsLink: '/advanced-algorithms-and-problem-solving',
-  },
-  // {
-  //   title: '',
-  //   subTitle: '',
-  //   tags: [],
-  //   description: '',
-  //   detailsLink: '',
-  // },
-];
+import ShortId from 'shortid';
+import courses from './data';
 
 export default () => (
   <section className="section section-lg pt-lg-0 mt--200">
@@ -44,34 +11,54 @@ export default () => (
           <h2 className="text-white text-center">Courses Offered</h2>
           <div className="row row-grid">
             {courses.map(
-              ({ title, subTitle, tags, description, detailsLink }) => (
-                <div className="col-lg-6 d-flex">
-                  <div className="card shadow-lg--hover border-2">
+              ({
+                title,
+                subTitle,
+                tags,
+                description,
+                detailsLink,
+                isBlurred,
+              }) => (
+                <div key={ShortId.generate()}className="col-lg-6 d-flex">
+                  <div className="card shadow-lg--hover border-2 mb-4">
                     <div className="card-body py-5">
                       <div>
-                        <span className="badge badge-pill badge-primary">
+                        <span className={`badge badge-pill badge-primary`}>
                           <strong>{subTitle}</strong>
                         </span>
                       </div>
-                      <h5 className="text-primary text-uppercase">{title}</h5>
-                      <p className="description mt-3 font-weight-normal">
+                      <h5 className={`text-primary text-uppercase`}>{title}</h5>
+                      <p
+                        className={`description mt-3 font-weight-normal ${
+                          isBlurred ? 'blur-card' : ''
+                        }`}
+                      >
                         {description}
                       </p>
                       <div>
                         {tags.map(tag => (
-                          <span className="badge badge-pill badge-primary mx-1">
+                          <span key={ShortId.generate()}
+                            className={`badge badge-pill badge-primary mx-1 ${
+                              isBlurred ? 'blur-card' : ''
+                            }`}
+                          >
                             {tag}
                           </span>
                         ))}
                       </div>
-                      <Link to={detailsLink} className="btn btn-primary mt-4">
+                      <Link
+                        to={detailsLink}
+                        className={`btn btn-primary mt-4 ${
+                          isBlurred ? 'blur-card' : ''
+                        }`}
+                      >
                         Learn more
                       </Link>
                       <a
                         href="https://coderhive.typeform.com/to/HswM5Q"
-                        className="btn btn-danger mt-4"
+                        className={`btn btn-danger mt-4`}
                       >
-                        Apply Now
+                        {isBlurred ? 'Coming Soon' : 'Apply Now'}
                       </a>
                     </div>
                   </div>
